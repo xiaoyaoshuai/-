8fcb0317b411ca6df796fe019b51c2011f4cbed0
ws <xiaoyaoshuai@outlook.com>
2018-08-01 20:15:32 +0800

    网吧终极版

---
+++ 程序/shuju.py
@@ -0,0 +1,74 @@
+#输入正确才可进入系统
+id_card = []
+def show_menu():#显示菜单
+    print('*' * 50)
+    print('欢迎使网吧管理系统S1.0')
+    print('1.新建会员\n2.显示所有会员\n3.搜索会员\n4.删除会员\n5.修改会员\n6.保存会员数据\n7.显示帮助信息\n8.开户上机\n0.退出系统:')
+    print('*' * 50)
+def show_help():#显示帮助
+    print('只有管理者才可以登录此系统')
+    print('此系统由逍遥帅开发')
+def new_card():#新建会员
+    name = input('请输入姓名:')
+    card = input('请输入身份证号码:')
+    phone = input('请输入手机号:')
+    money = input('请输入充值金额(元):')
+    zd = {}
+    zd['name'] = name
+    zd['card'] = card
+    zd['phone'] = phone
+    zd['money'] = money
+    id_card.append(zd)
+def show_card():#显示所有会员
+    print('*'*50)
+    print('会员信息如下:')
+    print('序号  姓名  身份证号码      手机号   预留金额')
+    i = 1
+    for temp in id_card:
+        print('%d   %s    %s   %s  %s'%(i,temp['name'],temp['card'],temp['phone'],temp['money']))
+        i += 1
+def search_card():#搜索会员
+    find_name = input('请输入要查询的名字:')
+    for zd in id_card:
+        if find_name == zd['name']:
+             print('姓名   身份证号码    电话    预留金额')
+             print('*' * 30)
+             print('%s   %s   %s   %s'%(zd['name'],zd['card'],zd['phone'],zd['money']))
+             break
+def del_card(member):#删除会员
+    del_card = int(input('请输入要删除的会员序号')) - 1
+    del member[del_card]
+def change_card():#修改会员
+    card_id = int(input('请输入修改的会员的序号":'))
+    name = input ('请输入新会员的名字:')
+    card = input ('请输入新会员身份证号码:')
+    phone = input('请输入新会员的手机号:')
+    money = input('请输入新会员充值金额(元):')
+    id_card[card_id-1]['name'] = name
+    id_card[card_id-1]['card'] = card
+    id_card[card_id-1]['phone'] = phone
+    id_card[card_id-1]['money'] = money
+def save_to_file():#保存到本地
+    file = open('new_card.data','w')
+    file.write(str(id_card))
+    file.close()
+def recover_data():
+    global id_card
+    try:
+        file = open('new_card.data')
+        content = file.read()
+        id_card = eval(content)
+        file.close()
+    except:
+        print('没有记录')
+def cz_money():#登陆上机
+    sj_name = input('请输入上机人姓名')
+    for zd  in id_card:
+        s = zd['money']
+        if sj_name == zd['name']:
+            a = input('%s是否需要激活上机？(Yes or No)'%sj_name)
+            if a == 'Yes':
+                print('欢迎登录，%s的剩余金额为%s(元)'%(sj_name,s))
+            else:
+                print('欢迎下次再来')
+
---
+++ 程序/yxcx.py
@@ -0,0 +1,48 @@
+import shuju
+def ws(fun):
+    def kf():
+        i =1
+        while i<=3:
+            zh = input('请输入账号')
+            pwd = input('请输入密码')
+            zh1 = 'xiaoyao'
+            pwd1 = '1101'
+            if zh == zh1 and pwd == pwd1:
+                print('登陆成功')
+                fun()
+                break
+            else:
+                print('密码输入错误，请重试')
+            i += 1
+        if i == 4:
+            print('输入次数过多，电脑将自动锁')
+    return kf
+@ws    
+def main():#运行
+    shuju.recover_data()
+    while True:
+        shuju.show_menu()
+        key = input('请输入你要选择的功能:')
+        if key == '1':
+            shuju.new_card()
+        elif key == '2':
+            shuju.show_card()
+        elif key == '3':
+            shuju.search_card()
+        elif key == '4':
+            shuju.del_card()
+        elif key == '5':
+            shuju.change_card()
+        elif key == '6':
+            shuju.save_to_file()
+        elif key == '7':
+            shuju.show_help()
+        elif key == '8':
+            shuju.cz_money()
+        elif key == '0':
+            quit = input('亲，真的要退出么?:(Yes or No)')
+            if quit == 'Yes':
+                print('已退出')
+                break
+main()
+
